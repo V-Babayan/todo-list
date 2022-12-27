1a7bcb639c1653edae1f13bcc592aefe17b1c0bb
V-Babayan <vladislavbabayan@gmail.com>
2022-12-27 20:12:29 +0400

    fix: add checking algorithm for correct working

--- src/helpers/dateWorking.js
+++ src/helpers/dateWorking.js
@@ -1,11 +1,14 @@
 export const dateToString = (date) => {
-  let month = date.getMonth();
+  let month = date.getMonth(),
+    day = date.getDate();
 
   if (month > 8) {
     month = month + 1;
   } else month = "0" + (month + 1);
 
-  let result = date.getFullYear() + "-" + month + "-" + date.getDate();
+  if (day < 10) day = "0" + day;
+
+  let result = [date.getFullYear(), month, day].join("-");
 
   return result;
 };
