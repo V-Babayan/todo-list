1a1f6892ae2b94e92761373f9184359aba1a2220
V-Babayan <vladislavbabayan@gmail.com>
2022-12-28 13:16:16 +0400

    docs: add react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,117 +1,26 @@
-import React, { memo, useState } from "react";
+import React from "react";
+import { Routes, Route, NavLink } from "react-router-dom";
+import General from "./pages/General";
+import Archive from "./pages/Archive";
+import Trash from "./pages/Trash";
+import Login from "./pages/Login";
 
-import Modal from "./components/modal/Modal";
-import TodoList from "./components/todo-list/TodoList";
-import ModalForm from "./components/modal-form/ModalForm";
-import { useTodos } from "./hooks/useTodos";
-import FilterForm from "./components/filter-form/FilterForm";
-
-let currentTodo;
-
-function App() {
-  const [todos, setTodos] = useState([
-    {
-      id: 1,
-      title: "Todos project is completed again over of dead line",
-      description: "Create react app 'Todos list'.",
-      created: new Date(2022, 2, 12),
-      expected: new Date(2022, 3, 12),
-      priority: "low",
-      isActive: true,
-    },
-    {
-      id: 2,
-      title: "Calculator",
-      description: "Create react app 'Calculator'.",
-      created: new Date(2022, 2, 13),
-      expected: new Date(2022, 2, 18),
-      priority: "high",
-      isActive: false,
-    },
-    {
-      id: 3,
-      title: "Physics",
-      description: "Run 5kms.",
-      created: new Date(2022, 2, 18),
-      expected: new Date(2022, 3, 1),
-      priority: "medium",
-      isActive: true,
-    },
-    {
-      id: 4,
-      title: "Practics",
-      description: "Practics react styled-components.",
-      created: new Date(2022, 2, 10),
-      expected: new Date(2022, 2, 15),
-      priority: "low",
-      isActive: false,
-    },
-  ]);
-  const [modalActive, setModalActive] = useState(false);
-  const [filter, setFilter] = useState({
-    sort: "",
-    query: "",
-  });
-  const sortedAndSearchedTodos = useTodos(filter, todos);
-
-  const changeIsActive = (id) => {
-    setTodos(
-      todos.map((item) => {
-        if (item.id === id) item.isActive = !item.isActive;
-        return item;
-      })
-    );
-  };
-
-  const changeTodoModal = (item) => {
-    currentTodo = item;
-    setModalActive(true);
-  };
-
-  const changingTodo = (item) => {
-    setTodos(
-      todos.map((elem) => {
-        if (elem.id === item.id) return item;
-        return elem;
-      })
-    );
-    setModalActive(false);
-    currentTodo = {};
-  };
-
-  const creatingHundle = () => {
-    currentTodo = {};
-    setModalActive(true);
-  };
-
-  const createItem = (item) => {
-    setTodos([...todos, { ...item, id: Date.now(), isActive: true }]);
-    setModalActive(false);
-  };
-
-  const removeTodo = () => {
-    setTodos(todos.filter((elem) => elem.id !== currentTodo.id));
-    setModalActive(false);
-  };
-
+const App = () => {
   return (
-    <div>
-      <FilterForm filter={filter} setFilter={setFilter} create={creatingHundle} />
-      <TodoList
-        todos={sortedAndSearchedTodos}
-        change={changeTodoModal}
-        changeIsActive={changeIsActive}
-      />
-      <Modal active={modalActive} setActive={setModalActive}>
-        <ModalForm
-          item={currentTodo}
-          create={createItem}
-          change={changingTodo}
-          remove={removeTodo}
-        />
-      </Modal>
-    </div>
+    <>
+      <header>
+        <NavLink to={"/general"}>General</NavLink>
+        <NavLink to={"/archive"}>Archive</NavLink>
+        <NavLink to={"/trash"}>Trash</NavLink>
+      </header>
+      <Routes>
+        <Route path='/general' element={<General />} />
+        <Route path='/archive' element={<Archive />} />
+        <Route path='/trash' element={<Trash />} />
+        <Route path='/login' element={<Login />} />
+      </Routes>
+    </>
   );
-}
+};
 
-export default memo(App);
+export default App;
--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { ThemeProvider } from "styled-components";
-
+import { BrowserRouter as Router } from "react-router-dom";
 import theme, { GlobalStyles } from "./theme";
 
 import App from "./App";
@@ -9,7 +9,9 @@ import App from "./App";
 const root = createRoot(document.getElementById("root"));
 root.render(
   <ThemeProvider theme={theme}>
-    <GlobalStyles />
-    <App />
+    <Router>
+      <GlobalStyles />
+      <App />
+    </Router>
   </ThemeProvider>
 );
