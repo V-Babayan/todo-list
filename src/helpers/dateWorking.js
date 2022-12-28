ff8c1faa103657eee0c3d140cfe08aebbe77d7a3
V-Babayan <vladislavbabayan@gmail.com>
2022-12-28 12:07:30 +0400

    refactor: change dateToString alghorithm

--- src/helpers/dateWorking.js
+++ src/helpers/dateWorking.js
@@ -1,14 +1,17 @@
 export const dateToString = (date) => {
-  let month = date.getMonth(),
-    day = date.getDate();
+  // let month = date.getMonth(),
+  //   day = date.getDate();
 
-  if (month > 8) {
-    month = month + 1;
-  } else month = "0" + (month + 1);
+  // if (month > 8) {
+  //   month = month + 1;
+  // } else month = "0" + (month + 1);
 
-  if (day < 10) day = "0" + day;
+  // if (day < 10) day = "0" + day;
 
-  let result = [date.getFullYear(), month, day].join("-");
+  // let result = [date.getFullYear(), month, day].join("-");
+
+  const result = date.toISOString().slice(0, 10);
+  console.log(result);
 
   return result;
 };
