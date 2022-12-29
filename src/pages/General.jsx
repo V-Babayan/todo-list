28b4bdf393a51d0cc306d32b930e053268fded35
V-Babayan <vladislavbabayan@gmail.com>
2022-12-29 17:15:26 +0400

    chore: change routing structure

--- src/pages/General.jsx
+++ src/pages/General.jsx
@@ -1,57 +1,60 @@
-import React, { memo, useState } from "react";
+import React, { memo, useState, useMemo } from "react";
 
 import Modal from "../components/modal/Modal";
 import TodoList from "../components/todo-list/TodoList";
 import ModalForm from "../components/modal-form/ModalForm";
 import { useTodos } from "../hooks/useTodos";
 import FilterForm from "../components/filter-form/FilterForm";
+import Header from "../components/header/Header";
 
-let currentTodo;
+const initialTodos = [
+  {
+    id: 1,
+    title: "Todos project is completed again over of dead line",
+    description: "Create react app 'Todos list'.",
+    created: new Date(2022, 2, 12),
+    expected: new Date(2022, 3, 12),
+    priority: "low",
+    isActive: true,
+  },
+  {
+    id: 2,
+    title: "Calculator",
+    description: "Create react app 'Calculator'.",
+    created: new Date(2022, 2, 13),
+    expected: new Date(2022, 2, 18),
+    priority: "high",
+    isActive: false,
+  },
+  {
+    id: 3,
+    title: "Physics",
+    description: "Run 5kms.",
+    created: new Date(2022, 2, 18),
+    expected: new Date(2022, 3, 1),
+    priority: "medium",
+    isActive: true,
+  },
+  {
+    id: 4,
+    title: "Practics",
+    description: "Practics react styled-components.",
+    created: new Date(2022, 2, 10),
+    expected: new Date(2022, 2, 15),
+    priority: "low",
+    isActive: false,
+  },
+];
 
 function General() {
-  const [todos, setTodos] = useState([
-    {
-      id: 1,
-      title: "Todos project is completed again over of dead line",
-      description: "Create react app 'Todos list'.",
-      created: new Date(2022, 2, 12),
-      expected: new Date(2022, 3, 12),
-      priority: "low",
-      isActive: true,
-    },
-    {
-      id: 2,
-      title: "Calculator",
-      description: "Create react app 'Calculator'.",
-      created: new Date(2022, 2, 13),
-      expected: new Date(2022, 2, 18),
-      priority: "high",
-      isActive: false,
-    },
-    {
-      id: 3,
-      title: "Physics",
-      description: "Run 5kms.",
-      created: new Date(2022, 2, 18),
-      expected: new Date(2022, 3, 1),
-      priority: "medium",
-      isActive: true,
-    },
-    {
-      id: 4,
-      title: "Practics",
-      description: "Practics react styled-components.",
-      created: new Date(2022, 2, 10),
-      expected: new Date(2022, 2, 15),
-      priority: "low",
-      isActive: false,
-    },
-  ]);
+  const [todos, setTodos] = useState(initialTodos);
   const [modalActive, setModalActive] = useState(false);
   const [filter, setFilter] = useState({
     sort: "",
     query: "",
   });
+  const [currentTodo, setCurrentTodo] = useState({});
+
   const sortedAndSearchedTodos = useTodos(filter, todos);
 
   const changeIsActive = (id) => {
@@ -64,39 +67,42 @@ function General() {
   };
 
   const changeTodoModal = (item) => {
-    currentTodo = item;
+    setCurrentTodo(item);
     setModalActive(true);
   };
 
-  const changingTodo = (item) => {
+  const isCreate = useMemo(() => Object.keys(currentTodo).length === 0, [modalActive]);
+
+  const modalOpenAndClose = (modalValue) => {
+    setCurrentTodo({});
+    setModalActive(modalValue);
+  };
+
+  const changingTodo = () => {
     setTodos(
       todos.map((elem) => {
-        if (elem.id === item.id) return item;
+        if (elem.id === currentTodo.id) return currentTodo;
         return elem;
       })
     );
-    setModalActive(false);
-    currentTodo = {};
+    modalOpenAndClose(false);
   };
 
-  const creatingHundle = () => {
-    currentTodo = {};
-    setModalActive(true);
-  };
-
-  const createItem = (item) => {
-    setTodos([...todos, { ...item, id: Date.now(), isActive: true }]);
-    setModalActive(false);
+  const createItem = () => {
+    if (!currentTodo.id) currentTodo.id = Date.now();
+    setTodos([...todos, { ...currentTodo, isActive: true }]);
+    modalOpenAndClose(false);
   };
 
   const removeTodo = () => {
-    setTodos(todos.filter((elem) => elem.id !== currentTodo.id));
-    setModalActive(false);
+    setTodos(todos.filter((item) => item.id !== currentTodo.id));
+    modalOpenAndClose(false);
   };
 
   return (
     <div>
-      <FilterForm filter={filter} setFilter={setFilter} create={creatingHundle} />
+      <Header />
+      <FilterForm filter={filter} setFilter={setFilter} create={() => modalOpenAndClose(true)} />
       <TodoList
         todos={sortedAndSearchedTodos}
         change={changeTodoModal}
@@ -104,10 +110,12 @@ function General() {
       />
       <Modal active={modalActive} setActive={setModalActive}>
         <ModalForm
-          item={currentTodo}
+          currentTodo={currentTodo}
+          setCurrentTodo={setCurrentTodo}
           create={createItem}
           change={changingTodo}
           remove={removeTodo}
+          isCreate={isCreate}
         />
       </Modal>
     </div>
--- src/pages/Trash.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Trash = () => {
-  return <div>Trash page</div>;
-};
-
-export default Trash;
