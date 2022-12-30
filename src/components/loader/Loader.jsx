e0907c0680fc246322c8fc5575e4e6d0d393ecea
V-Babayan <vladislavbabayan@gmail.com>
2022-12-30 14:47:46 +0400

    feat: add new component noader

---
+++ src/components/loader/Loader.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { StyledLoader, StyledLoaderWrapper } from "./Loader.styled.js";
+
+const Loader = () => {
+  return (
+    <StyledLoaderWrapper>
+      <StyledLoader />
+    </StyledLoaderWrapper>
+  );
+};
+
+export default Loader;
---
+++ src/components/loader/Loader.styled.js
@@ -0,0 +1,32 @@
+import styled, { keyframes } from "styled-components";
+
+const loading = keyframes`
+  from {
+    transform: rotateZ(0) scale(1);
+  }
+
+  50% {
+    transform: rotateZ(90deg) scale(1.3);
+  }
+
+  to {
+    transform: rotateZ(180deg) scale(1);
+  }
+`;
+
+export const StyledLoaderWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const StyledLoader = styled.div`
+  width: 150px;
+  height: 150px;
+  margin-top: 20px;
+
+  border-radius: 50%;
+  border: 10px dotted ${({ theme }) => theme.primary.green};
+
+  animation: ${loading} 2s linear infinite;
+`;
