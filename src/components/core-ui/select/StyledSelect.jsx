a4658853ee016607b52db073afac9ad75f6a58e6
V-Babayan <vladislavbabayan@gmail.com>
2023-01-03 16:14:35 +0400

    feat: change select component styles

--- src/components/core-ui/select/StyledSelect.jsx
+++ src/components/core-ui/select/StyledSelect.jsx
@@ -1,8 +1,34 @@
 import styled from "styled-components";
 
 export const StyledSelect = styled.select`
-  padding: 5px 10px;
+  border-radius: 3px;
+  border: 1px solid #7aa7c7;
+  box-shadow: rgba(255, 255, 255, 0.7) 0 1px 0 0 inset;
+  padding: 8px 0.8em;
 
-  border: 4px solid ${({ theme }) => theme.primary.green};
-  border-radius: 5px;
+  font-size: 13px;
+  line-height: 1.15385;
+  color: #39739d;
+  text-align: center;
+
+  transition: all 0.2s linear;
+
+  background-color: #e1ecf4;
+  cursor: pointer;
+
+  &:hover,
+  &:focus {
+    background-color: #b3d3ea;
+    color: #2c5777;
+  }
+
+  &:focus {
+    box-shadow: 0 0 0 4px rgba(0, 149, 255, 0.15);
+  }
+
+  &:active {
+    background-color: #a0c7e4;
+    box-shadow: none;
+    color: #2c5777;
+  }
 `;
