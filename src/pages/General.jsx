3e8af83a08437a1787d1f89f9ddce70ecf62d248
V-Babayan <vladislavbabayan@gmail.com>
2023-01-03 16:48:44 +0400

    feat: add new call function for fetching

--- src/pages/General.jsx
+++ src/pages/General.jsx
@@ -1,4 +1,4 @@
-import React, { memo, useState, useMemo } from "react";
+import React, { memo, useState, useMemo, useEffect } from "react";
 
 import Modal from "../components/modal/Modal";
 import TodoList from "../components/todo-list/TodoList";
@@ -6,76 +6,46 @@ import ModalForm from "../components/modal-form/ModalForm";
 import { useTodos } from "../hooks/useTodos";
 import FilterForm from "../components/filter-form/FilterForm";
 import Header from "../components/header/Header";
-
-const initialTodos = [
-  {
-    id: 1,
-    title: "Todos project is completed again over of dead line",
-    description: "Create react app 'Todos list'.",
-    created: new Date(2022, 2, 12),
-    expected: new Date(2022, 3, 12),
-    priority: "low",
-    isActive: true,
-  },
-  {
-    id: 2,
-    title: "Calculator",
-    description: "Create react app 'Calculator'.",
-    created: new Date(2022, 2, 13),
-    expected: new Date(2022, 2, 18),
-    priority: "high",
-    isActive: false,
-  },
-  {
-    id: 3,
-    title: "Physics",
-    description: "Run 5kms.",
-    created: new Date(2022, 2, 18),
-    expected: new Date(2022, 3, 1),
-    priority: "medium",
-    isActive: true,
-  },
-  {
-    id: 4,
-    title: "Practics",
-    description: "Practics react styled-components.",
-    created: new Date(2022, 2, 10),
-    expected: new Date(2022, 2, 15),
-    priority: "low",
-    isActive: false,
-  },
-];
+import { useFetching } from "../hooks/useFetching";
 
 function General() {
-  const [todos, setTodos] = useState(initialTodos);
+  const [todos, setTodos] = useState([]);
   const [modalActive, setModalActive] = useState(false);
   const [filter, setFilter] = useState({
     sort: "",
     query: "",
   });
   const [currentTodo, setCurrentTodo] = useState({});
+  const [fetching, isLoading, error] = useFetching(async () => {
+    const data = await (await fetch("https://jsonplaceholder.typicode.com/todos")).json();
+    setTodos(data);
+  });
 
   const sortedAndSearchedTodos = useTodos(filter, todos);
 
-  const changeIsActive = (id) => {
+  useEffect(() => {
+    fetching();
+  }, []);
+
+  const changeCompleted = (id) => {
     setTodos(
       todos.map((item) => {
-        if (item.id === id) item.isActive = !item.isActive;
+        if (item.id === id) item.completed = !item.completed;
         return item;
       })
     );
   };
 
-  const changeTodoModal = (item) => {
-    setCurrentTodo(item);
-    setModalActive(true);
-  };
-
   const isCreate = useMemo(() => Object.keys(currentTodo).length === 0, [modalActive]);
 
-  const modalOpenAndClose = (modalValue) => {
-    setCurrentTodo({});
+  const modalOpenAndClose = (modalValue, todo = {}) => {
+    setCurrentTodo(todo);
     setModalActive(modalValue);
+  };
+
+  const createTodo = () => {
+    setTodos([...todos, { ...currentTodo, id: Date.now(), completed: true }]);
+    modalOpenAndClose(false);
   };
 
   const changingTodo = () => {
@@ -88,11 +58,6 @@ function General() {
     modalOpenAndClose(false);
   };
 
-  const createItem = () => {
-    setTodos([...todos, { ...currentTodo, id: Date.now(), isActive: true }]);
-    modalOpenAndClose(false);
-  };
-
   const removeTodo = () => {
     setTodos(todos.filter((item) => item.id !== currentTodo.id));
     modalOpenAndClose(false);
@@ -100,18 +65,20 @@ function General() {
 
   return (
     <div>
-      <Header />
-      <FilterForm filter={filter} setFilter={setFilter} create={() => modalOpenAndClose(true)} />
+      <Header modalOpenAndClose={modalOpenAndClose} />
+      <FilterForm filter={filter} setFilter={setFilter} />
+
       <TodoList
+        isLoading={isLoading}
         todos={sortedAndSearchedTodos}
-        change={changeTodoModal}
-        changeIsActive={changeIsActive}
+        modalOpenAndClose={modalOpenAndClose}
+        changeCompleted={changeCompleted}
       />
       <Modal active={modalActive} setActive={setModalActive}>
         <ModalForm
           currentTodo={currentTodo}
           setCurrentTodo={setCurrentTodo}
-          create={createItem}
+          create={createTodo}
           change={changingTodo}
           remove={removeTodo}
           isCreate={isCreate}
