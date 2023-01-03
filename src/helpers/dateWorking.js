2471aab1ba4edfa3ff0d3de8953f4dc503937e38
V-Babayan <vladislavbabayan@gmail.com>
2023-01-03 16:47:38 +0400

    feat: add checking for parameter of function be have

--- src/helpers/dateWorking.js
+++ src/helpers/dateWorking.js
@@ -1,3 +1,19 @@
+/**
+ *
+ * @param { DateConstructor } date
+ * @returns string
+ */
 export const dateToString = (date) => {
-  if (date) return date.toISOString().slice(0, 10);
+  if (!date) {
+    throw new Error("Date is required for converting process.");
+  }
+
+  return date.toISOString().slice(0, 10);
 };
+
+/**
+ *
+ * @param { DateConstructor } date
+ * @returns string
+ */
+export const dateToLocalString = (date) => date.toLocaleString("en-GB").slice(0, 10);
