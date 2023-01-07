7c447a1b4b3c3a5f2b17733024c7945975ae1f21
V-Babayan <vladislavbabayan@gmail.com>
2023-01-07 22:08:50 +0400

    feat: add new modal working function

--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -3,22 +3,35 @@ import React, { useRef } from "react";
 import { StyledModal, StyledModalContent } from "./Modal.styled";
 import { Transition } from "react-transition-group";
 
-const Modal = ({ active, setActive, children }) => {
+import ModalStore from "../../store/ModalStore";
+import Todo from "../../store/Todo";
+import { observer } from "mobx-react-lite";
+
+const Modal = observer(({ children }) => {
   const nodeRef = useRef(null);
   const handleClick = (event) => {
     event.stopPropagation();
-    setActive(false);
+    ModalStore.toggleModal();
+    Todo.setCurrentTodo({});
   };
 
   return (
-    <Transition nodeRef={nodeRef} in={active} timeout={500} mountOnEnter unmountOnExit>
+    <Transition
+      nodeRef={nodeRef}
+      in={ModalStore.active}
+      timeout={500}
+      mountOnEnter
+      unmountOnExit>
       {(state) => (
-        <StyledModal ref={nodeRef} onClick={handleClick} className={`${state}`}>
+        <StyledModal
+          ref={nodeRef}
+          onClick={handleClick}
+          className={`${state}`}>
           <StyledModalContent onClick={(e) => e.stopPropagation()}>{children}</StyledModalContent>
         </StyledModal>
       )}
     </Transition>
   );
-};
+});
 
 export default Modal;
