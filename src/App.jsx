9b5e287958267fb6154a0c661a1619cf323c62fc
V-Babayan <vladislavbabayan@gmail.com>
2023-01-11 11:51:29 +0400

    refactore: move AppRouter component to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { Suspense } from "react";
+import { Route, Routes } from "react-router-dom";
+
+import routes from "./routes";
 import Header from "./components/header/Header";
-import AppRouter from "./components/AppRouter";
+import Loader from "./components/loader/Loader";
 
 const App = () => {
   return (
     <>
       <Header />
-      <AppRouter />
+      <Routes>
+        {routes.map(({ path, Component }, index) => (
+          <Route
+            key={index}
+            path={path}
+            element={
+              <Suspense fallback={<Loader />}>
+                <Component />
+              </Suspense>
+            }
+          />
+        ))}
+      </Routes>
     </>
   );
 };
